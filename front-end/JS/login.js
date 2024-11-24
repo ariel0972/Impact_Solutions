103cc6f48cbd38cd4f750442d8af72e98ba4a8ba
Ariel_EA <are3380@gmail.com>
2024-11-23 22:49:14 -0300

    Arrumando o login e cadastroJS
    HIGOT SÓ FAZ MERDA PQP

--- front-end/JS/login.js
+++ front-end/JS/login.js
@@ -1,65 +1,76 @@
 ﻿window.onload = function (e) {
-    var txtEmail = document.getElementById("txtEmail");
+
+    var btnEntrar = document.getElementById("enter");
+    var txtEmail = document.getElementById("email");
+    var txtSenha = document.getElementById("password");
 
     txtEmail.focus();
 
-    var form = document.getElementById("frmEsqueceu");
+    function fazerLogin(email, senha) {
+        var usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+    
+        var usuario = usuarios.find(function(usuario) {
+            return usuario.email === email;
+        });
+    
+        if (usuario && usuario.senha === senha) {
+            return { sucesso: true, mensagem: "Login bem-sucedido.", usuario: usuario};
+        } else {
+            exibirMensagemErro("Email ou senha incorretos");
+        }
+    }
+    
 
-    form.onsubmit = function (e) {
-
+    btnEntrar.onclick = function (e) {
         e.preventDefault();
 
         var email = txtEmail.value;
+        var senha = txtSenha.value;
 
         if (email == "") {
-
-            var mensagem = "E-mail obrigatório.";
-
-            exibirMensagemErro(mensagem)
+            exibirMensagemErro("Campo E-mail obrigatório.");
+        }
+        else if (senha == "") {
+            exibirMensagemErro("Campo Senha obrigatório.");
         }
         else {
-
-            esqueceuSenha(email);
+            var resultado = fazerLogin(email, senha);
+            if (resultado.sucesso) {
+                window.location.href = "http://localhost:5501/front-end/html/perfil_ong.html";
+            } else {
+                alert(resultado.mensagem);
+            }
         }
     };
 
-    function esqueceuSenha(email) {
-        var data = JSON.stringify({
-            "email": email
-        });
-
-        var xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
-
-        xhr.addEventListener("readystatechange", function () {
-            if (this.readyState === 4) {
-
-                var result = JSON.parse(this.responseText);
-
-                if (result.sucesso) {
-                   alert(" Email enviado com sucesso!");
-                }
-                else {
-                    exibirMensagemErro(result.mensagem);
-                }
-            }
-        });
-
-        xhr.open("POST", "http://localhost:21399/api/usuario/esqueceuSenha");
-        xhr.setRequestHeader("Content-Type", "application/json");
-
-        xhr.send(data);
-    }
+    //função pra exibir o erro na caixa de login
     function exibirMensagemErro(mensagem) {
 
         var spnErro = document.getElementById("spnErro");
 
         spnErro.innerText = mensagem;
-
         spnErro.style.display = "block";
-
         setTimeout(function () {
             spnErro.style.display = "none";
         }, 5000);
+        
     }
+
+    document.getElementById("formLogin").onsubmit = function(e) {
+        e.preventDefault();
+    
+        var email = document.getElementById("email").value;
+        var senha = document.getElementById("password").value;
+    
+        var resultado = fazerLogin(email, senha);
+    
+        if (resultado.sucesso) {
+            // Login bem-sucedido, redirecione para a página de tarefas
+            window.location.href = "http://localhost:5501/front-end/html/perfil_ong.html";
+        } else {
+            // Exiba mensagem de erro
+            alert(resultado.mensagem);
+        }
+        
+    };
 }
